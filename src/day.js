0c18f69a7eaf181f740035e07a1e1b28d1c6d41d
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-10-23 09:50:17 -0400

    added bootstrap

---
+++ src/day.js
@@ -0,0 +1,25 @@
+
+"id": 1,
+"name": "Monday",
+"date": "2018-10-18",
+"notes": [
+{
+"id": 1,
+"name": "Fishing",
+"description": "we going fishing at this day",
+"day_id": 1
+}
+]
+
+let calendar = { days: [], passengers: []}
+
+class Day {
+
+    constructor(id,name,date){
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+
+}
