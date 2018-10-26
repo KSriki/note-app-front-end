7b35616a83850798b886d52cb76be974b1a6b4f3
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-10-25 21:34:04 -0400

    list of notes

--- src/calendar.js
+++ src/calendar.js
@@ -1,118 +1,136 @@
-class Calendar{
+class Calendar {
 
-
-    static RenderCalendar(){
-      let container = $('#lobby-container')
-      // container.html('')
-      let calendarDiv = $('<div>')
-      calendarDiv.attr("id", "calendar")
-      container.append(calendarDiv)
-      calendarDiv.fullCalendar({
-        header: {
-          center: 'agendaSevenDay,month' // buttons for switching between views
-        },
-        views: {
-          agendaSevenDay: {
-            type: 'agenda',
-            duration: { days: 7 },
-            buttonText: '7 day'
-          }
-        }
-      });
-      Calendar.loadNotes();
+    static RenderCalendar() {
+        let container = $('#lobby-container')
+        // container.html('')
+        let calendarDiv = $('<div>')
+        calendarDiv.attr("id", "calendar")
+        container.append(calendarDiv)
+        calendarDiv.fullCalendar({
+            header: {
+                center: 'agendaSevenDay,month' // buttons for switching between views
+            },
+            views: {
+                agendaSevenDay: {
+                    type: 'agenda',
+                    duration: {
+                        days: 7
+                    },
+                    buttonText: '7 day'
+                }
+            }
+        });
+        Calendar.loadNotes();
     }
 
-    static ToggleCalendar(){
-      let calendarButton = $('#calendarButton')
-      calendarButton.click(function(){
-        let container = $('#lobby-container')
-        container.html('')
-        if (calendarButton.html() === "Show My Calendar"){
-          calendarButton.html('Hide My Calendar')
-          Calendar.RenderCalendar()
-        }
-      else if (calendarButton.html() === "Hide My Calendar"){
-        calendarButton.html('Show My Calendar')
-        $('#calendar').hide()
-        container.append(`<div id="lobby-row" class="row"></div>`);
-        Controller.renderNotes(Calendar.all.notes)
-      }
-    })
-  }
+    static ToggleCalendar() {
+        let calendarButton = $('#calendarButton')
+        calendarButton.click(function() {
+            let container = $('#lobby-container')
+            container.html('')
+            if (calendarButton.html() === "Show My Calendar") {
+                calendarButton.html('Hide My Calendar')
+                Calendar.RenderCalendar()
+            } else if (calendarButton.html() === "Hide My Calendar") {
+                calendarButton.html('Show My Calendar')
+                $('#calendar').hide()
+                container.append(`<div id="lobby-row" class="row"></div>`);
+                Controller.renderNotes(Calendar.all.notes)
+            }
+        })
+    }
 
-  static findDay(day){
+    static findDay(day) {
 
-      return Calendar.all.days.find(d => {
-          return d.date === day
-      })
-  }
+        return Calendar.all.days.find(d => {
+            return d.date === day
+        })
+    }
 
-  static loadNotes(){
-      let cals = $(".fc-day")
+    static loadNotes() {
+        let cals = $(".fc-day")
 
-      let copy = Calendar.all.notes.map(note => {
+        let copy = Calendar.all.notes.map(note => {
 
-          if(note.dayId != 1){
-              return Object.assign({},note,{date:note.getDay().date,type:note.getType.name });
-          }
-          else{
-              return {};
-          }
-    }).filter(obj => {return !jQuery.isEmptyObject(obj)});
+            if (note.dayId != 1) {
+                return Object.assign({}, note, {
+                    date: note.getDay().date,
+                    type: note.getType.name
+                });
+            } else {
+                return {};
+            }
+        }).filter(obj => {
+            return !jQuery.isEmptyObject(obj)
+        });
 
-          // debugger;
-      copy.sort(function(c1,c2){
-          // debugger;
-          let d1 = new Date(c1.date);
-          let d2 = new Date(c2.date);
-          if(d1 > d2 ){
-              return 1;
-          }
-          else if(d1 < d2){
-              return -1;
-          }
-          else {
-              return 0;
-          }
+        // debugger;
+        copy.sort(function(c1, c2) {
+            // debugger;
+            let d1 = new Date(c1.date);
+            let d2 = new Date(c2.date);
+            if (d1 > d2) {
+                return 1;
+            } else if (d1 < d2) {
+                return -1;
+            } else {
+                return 0;
+            }
 
-      });
+        });
 
-      copy.forEach(function(note){
+        copy.forEach(function(note) {
 
-              let date = note.date
-              for(let i = 0; i < cals.length;i++){
-                  // debugger;
-                  if(cals[i].dataset.date === date){
+            let date = note.date
+            for (let i = 0; i < cals.length; i++) {
+                // debugger
+                if (cals[i].dataset.date === date) {
+                    cals[i].classList.add("card-body");
+                    //find again
+                    if (!cals[i].innerHTML) {
 
-                      //find again
-                      if(!cals[i].innerHTML){
-                      let inner = `<ul class="list-group" id="list-${note.id}">
-                                      <li class="list-group-item d-flex justify-content-between align-items-center">
-                                            ${note.name}
-                                        <span class="badge badge-primary badge-pill">${note.type}</span>
-                                      </li>
 
-                                    </ul>`
-                    cals[i].innerHTML = inner
+                        let listCal = document.createElement("ul");
+                        listCal.setAttribute("class","list-group")
+                        listCal.id= `list-${note.id}`
+
+                        cals[i].appendChild(listCal);
                     }
-                  }
-              }
 
-      });
-    //   debugger;
-    //   let cals = $(".fc-day")
-    //   Calendar.all.notes.forEach(function(note){
-    //       let date = note.getDay().date
-    //
-    //
-    //   })
-    //
-    //
-    //   for(let i = 0; i < cals.length; i++){
-	// console.log(cals[i].dataset)
-// }
-  }
+                    // debugger;
+                    let inner = document.createElement("li");
+                    inner.setAttribute("class","list-group-item d-flex justify-content-between align-items-center");
+
+                    inner.innerHTML = `${note.name}`;
+
+                        cals[i].children[0].appendChild(inner)
+
+                }
+            }
+
+        });
+        //   debugger;
+        //   let cals = $(".fc-day")
+        //   Calendar.all.notes.forEach(function(note){
+        //       let date = note.getDay().date
+        //
+        //
+        //   })
+        //
+        //
+        //   for(let i = 0; i < cals.length; i++){
+        // console.log(cals[i].dataset)
+        // }
+    }
 
 }
 
-Calendar.weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+Calendar.weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
